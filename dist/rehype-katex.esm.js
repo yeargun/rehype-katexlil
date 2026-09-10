dea756bb6dbf03cfe821f6388589646608ff0b71
yeargun <yeargun24+eddocuvm@gmail.com>
2026-09-10 14:12:04 +0000

    Refresh comparison pages with compiler drift and measured build provenance

--- dist/rehype-katex.esm.js
+++ dist/rehype-katex.esm.js
@@ -10,40 +10,40 @@ var a = (c) => {
   var b = c;
   return b == null && (b = {}), (l, m) => {
     visitParents(l, "element", (u, v) => {
-      var x = [], w = u.properties, y;
+      var y, x = [], w = u.properties;
       w != null && Array.isArray(w.className) && (x = w.className), w = !!x.includes("language-math"), y = !!x.includes("math-display");
       var z = !!x.includes("math-inline");
-      if (!(!w && !y && !z) && (z = v.length, x = void 0, z > 0 && (x = v[z - 1]), u.tagName == "code" && w && x != null && x.type == "element" && x.tagName == "pre" ? (w = void 0, z > 1 && (w = v[z - 2]), y = !0) : (w = x, x = u), !(w == null || !w))) {
+      if (!(!w && !y && !z) && (z = +v.length | 0, x = void 0, z > 0 && (x = v[z - 1]), u.tagName == "code" && w && x != null && x.type == "element" && x.tagName == "pre" ? (w = void 0, z > 1 && (w = v[z - 2]), y = !0) : (w = x, x = u), !(w == null || !w))) {
         z = toText(x, { whitespace: "pre" }) + "";
-        var yb;
+        var zb;
         try {
-          var zb = Object.assign({}, b);
-          zb.displayMode = y, zb.throwOnError = !0;
-          var Ab = katex.renderToString(z, zb);
-          yb = fromHtmlIsomorphic(Ab, { fragment: !0 }).children;
+          var Ab = Object.assign({}, b);
+          Ab.displayMode = y, Ab.throwOnError = !0;
+          var Bb = katex.renderToString(z, Ab);
+          zb = fromHtmlIsomorphic(Bb, { fragment: !0 }).children;
         } catch (A) {
-          var Z = void 0, Bb = v.slice();
-          Array.prototype.push.call(Bb, u);
-          var Cb = A.name.toLowerCase();
-          v = Bb;
+          var _ = void 0, Cb = v.slice();
+          Array.prototype.push.call(Cb, u);
+          var Db = A.name.toLowerCase();
+          v = Cb;
           var B = u.position;
-          m.message("Could not render math with KaTeX", { ancestors: v, cause: A, place: B, ruleId: Cb, source: "rehype-katex" });
+          m.message("Could not render math with KaTeX", { ancestors: v, cause: A, place: B, ruleId: Db, source: "rehype-katex" });
           try {
-            var Db = Object.assign({}, b);
-            Db.displayMode = y, Db.strict = "ignore", Db.throwOnError = !1;
-            var Eb = katex.renderToString(z, Db);
-            yb = fromHtmlIsomorphic(Eb, { fragment: !0 }).children;
+            var Eb = Object.assign({}, b);
+            Eb.displayMode = y, Eb.strict = "ignore", Eb.throwOnError = !1;
+            var Fb = katex.renderToString(z, Eb);
+            zb = fromHtmlIsomorphic(Fb, { fragment: !0 }).children;
           } catch {
-            var Fb = b.errorColor;
-            Fb = Fb || "#cc0000";
-            var Lb = "color:" + Fb;
-            yb = [{ type: "element", tagName: "span", properties: { className: ["katex-error"], style: Lb, title: A + "" }, children: [{ type: "text", value: z }] }];
+            var Gb = b.errorColor;
+            Gb = Gb || "#cc0000";
+            var Mb = "color:" + Gb;
+            zb = [{ type: "element", tagName: "span", properties: { className: ["katex-error"], style: Mb, title: A + "" }, children: [{ type: "text", value: z }] }];
           }
         }
-        Lb = w.children, u = [+Lb.indexOf(x) | 0, 1], v = yb.length;
-        for (var Mb = 0; Mb < v; )
-          Array.prototype.push.call(u, yb[Mb]), Mb = Mb + 1;
-        return Lb.splice.apply(Lb, u), SKIP;
+        Mb = w.children, u = [+Mb.indexOf(x) | 0, 1], v = +zb.length | 0;
+        for (var Nb = 0; Nb < v; )
+          Array.prototype.push.call(u, zb[Nb]), Nb += 1;
+        return Mb.splice.apply(Mb, u), SKIP;
       }
     });
   };
